75c20a82595d2e62aed99bbff7318c4118b6594e
Louie Alfonso <lfriasalfonso@gmail.com>
2025-02-21 12:01:48 +0000

    Initial commit

---
+++ backend/src/test/java/eth/system/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package eth.system.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
