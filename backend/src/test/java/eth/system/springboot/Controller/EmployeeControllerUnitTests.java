bb226ce4b3bc19ce7476057a1b638db70af83600
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-02 21:51:23 +0000

    Added Controller Unit Test Cases for Get All Employees & Get Employee By ID

--- backend/src/test/java/eth/system/springboot/Controller/EmployeeControllerUnitTests.java
+++ backend/src/test/java/eth/system/springboot/Controller/EmployeeControllerUnitTests.java
@@ -2,6 +2,7 @@ package eth.system.springboot.Controller;
 
 import eth.system.springboot.controller.EmployeeController;
 import eth.system.springboot.dto.EmployeeDto;
+import eth.system.springboot.entity.Employee;
 import eth.system.springboot.repository.EmployeeRepository;
 import eth.system.springboot.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -75,7 +80,6 @@ public class EmployeeControllerUnitTests {
         verify(employeeService, times(1)).createNewEmployee(inputEmployeeDto);
     }
 
-
     @Test
     @Order(2)
     @DisplayName("Test 2: Create New Employee - Null Input")
@@ -112,5 +116,70 @@ public class EmployeeControllerUnitTests {
         // Verify
         verify(employeeService, times(1)).createNewEmployee(inputEmployeeDto);
 
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test 4: Get All Employees - Success")
+    void getAllEmployees_Success() {
+
+        // Arrange
+        EmployeeDto employeeDto1 = new EmployeeDto();
+        employeeDto1.setFirstName("John");
+        employeeDto1.setLastName("Doe");
+        employeeDto1.setEmployeeNumber("SYS-1234-5678");
+        employeeDto1.setPosition("Software Engineer");
+        employeeDto1.setEmailAddress("johndoe@gmail.com");
+        employeeDto1.setPhoneNumber("07-1234-567890");
+        employeeDto1.setEmployeeStatus("Full-time");
+
+        EmployeeDto employeeDto2 = new EmployeeDto();
+        employeeDto2.setFirstName("Jane");
+        employeeDto2.setLastName("Doe");
+        employeeDto2.setEmployeeNumber("SYS-9012-3456");
+        employeeDto2.setPosition("Marketing Manager");
+        employeeDto2.setEmailAddress("janedoe@gmail.com");
+        employeeDto2.setPhoneNumber("07-9012-345678");
+        employeeDto2.setEmployeeStatus("Contract");
+
+        List<EmployeeDto> employeeLists = Arrays.asList(employeeDto1, employeeDto2);
+
+        when(employeeService.getAllEmployees()).thenReturn(employeeLists);
+
+        // Act
+        ResponseEntity<List<EmployeeDto>> response = employeeController.getAllEmployees();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(2, response.getBody().size());
+        assertTrue(response.getBody().contains(employeeDto1));
+        assertTrue(response.getBody().contains(employeeDto2));
+
+        // Verify
+        verify(employeeService, times(1)).getAllEmployees();
+    }
+
+
+    @Test
+    @Order(5)
+    @DisplayName("Test 5: Get Asset By Id - When Employee Exists")
+    void getEmployeeById(){
+        // Arrange
+        Long employeeId = 1L;
+        Employee mockEmployee = new Employee();
+        mockEmployee.setId(employeeId);
+        mockEmployee.setEmployeeNumber("SYS-3543-2759");
+
+        // Mock the behavior of employeeRepository.findAllById()
+        when(employeeRepository.findAllById(employeeId)).thenReturn(Optional.of(mockEmployee));
+
+        // Act
+        ResponseEntity<Employee> response = employeeController.getEmployeeById(employeeId);
+
+        // Assert
+        assertEquals(200, response.getStatusCodeValue()); // Verify status code is 200 OK
+        assertNotNull(response.getBody()); // Verify response body is not null
+        assertEquals(mockEmployee, response.getBody()); // Verify the response body matches the mock employee
     }
 }
