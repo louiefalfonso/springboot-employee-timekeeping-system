bfc4430689b199630e8056b647dc77db23aa5e6d
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-05 20:38:00 +0000

    Created Department Models, Services & Components

---
+++ frontend/src/app/features/employee/models/employee.models.ts
@@ -0,0 +1,10 @@
+export interface Employee{
+  id: string;
+  firstName: string;
+  lastName: string;
+  employeeNumber: string;
+  emailAddress: string;
+  phoneNumber: string;
+  employeeStatus : string;
+  dateOfBirth: string;
+}
