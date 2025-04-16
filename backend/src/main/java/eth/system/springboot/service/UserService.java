3c4989ea6eae82b2ed2a9addf80e9f30030e6a11
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-16 16:13:27 +0100

    Setup Spring Security & Jwt Authentication

---
+++ backend/src/main/java/eth/system/springboot/service/UserService.java
@@ -0,0 +1,26 @@
+package eth.system.springboot.service;
+
+import eth.system.springboot.entity.User;
+import eth.system.springboot.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
